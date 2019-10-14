a7efa78a17e723286ef9ad34280b75316a6da256
Stefan Schenk <Stefan_schenk@hotmail.com>
2019-10-15 00:20:11 +0200

    Create mad_scientist.sh

---
+++ mad_scientist.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Usage: curl https://pyenv.run | bash
+#
+# For more info, visit: https://raw.githubusercontent.com/pyenv/pyenv-installer
+#
+set -e
+
+echo "> Prechecks"
+if [[ -z "$http_proxy" ]]; then
+    echo "Please export http_proxy variable in your ~/.bashrc" 1>&2
+    exit 1
+fi
+if ! which git &>/dev/null; then 
+    echo "Please install git"
+    exit 1
+fi
+
+echo "> Setting apt, curl, and wget config settings"
+echo "Acquire::http::Proxy $http_proxy;" >> /etc/apt/apt.conf
+echo "check_certificate = off" >> ~/.wgetrc
+echo insecure >> ~/.curlrc
+
+echo "> Setting some ~/.bashrc commands"
+cat >>~/.bashrc <<EOL
+
+# Proxy settings
+export HTTP_PROXY=$http_proxy
+export HTTPS_PROXY=$http_proxy
+export FTP_PROXY=$http_proxy
+export DNS_PROXY=$http_proxy
+export RSYNC_PROXY=$http_proxy
+export ALL_PROXY=$http_proxy
+export NO_PROXY="localhost"
+
+# Pipenv
+export PIPENV_VENV_IN_PROJECT=1
+
+# Pyenv
+export PATH="/home/$USER/.pyenv/bin:$PATH"
+
+# For git only
+git config --global http.proxyAuthMethod 'basic'
+EOL
+
+echo "> Installing pyenv"
+curl -s -S -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
